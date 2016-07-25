b0b1981db8955462a4d275187e5ec702cb6b3c3f
Carlo Baldassi <carlobaldassi@gmail.com>
2016-07-25 17:11:30 +0200

    Add script in utils

---
+++ utils/paralog_matching.jl
@@ -0,0 +1,46 @@
+#!/usr/bin/env julia
+
+include("../src/ParalogMatching.jl")
+using .ParalogMatching
+using ArgParse
+
+function main()
+    s = ArgParseSettings("Match paralog species based on co-evolution signals.")
+
+    @add_arg_table s begin
+        "--cutoff", "-c"
+            arg_type = Int
+            default = 500
+            range_tester = (x->x≥0)
+            help = """maximum number of sequences per species
+                      (discards species with more sequences than the cutoff),
+                      use 0 to disable"""
+        "--batch", "-b"
+            arg_type = Int
+            default = 1
+            range_tester = (x->x≥1)
+            help = """batch size, i.e. number of species to match before updating
+                      the underlying alignment model (lower is more accurate but slower)"""
+        "infile1"
+            help = "first input alignment"
+            required = true
+        "infile2"
+            help = "second input alignment"
+            required = true
+        "outfile"
+            help = "output matched alignment"
+            required = true
+    end
+
+    parsed_args = parse_args(ARGS, s)
+
+    cutoff = parsed_args["cutoff"]
+    batch = parsed_args["batch"]
+    infile1 = parsed_args["infile1"]
+    infile2 = parsed_args["infile2"]
+    outfile = parsed_args["outfile"]
+
+    paralog_matching(infile1, infile2, outfile, cutoff=cutoff, batch=batch)
+end
+
+main()
