72730135885e9be22516dbdbeeee9b8fa4c57701
Thomas Gueudre <gueudre.t@gmail.com>
2016-07-28 14:00:27 +0200

    Amends the test file to accomodate the new default in pseudo_count

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,7 @@ const outfile_ok = "Xmatched.fasta.gz"
 function test()
     outfile_tst = tempname()
     try
-        paralog_matching(testfile1, testfile2, outfile_tst, cutoff=4, batch=5)
+        paralog_matching(testfile1, testfile2, outfile_tst, cutoff=4, pseudo_count=0.8,batch=5)
 
         M1 = readfasta(outfile_tst)
         M2 = readfasta(outfile_ok)
