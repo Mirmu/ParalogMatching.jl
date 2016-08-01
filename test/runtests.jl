e5f7fc774cedcad7be6df530155f93afac85bb22
pagnani <andrea.pagnani@gmail.com>
2016-08-01 18:44:28 +0200

    added test for Alignment constructor of HarmonizedAlignments

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,14 +10,19 @@ const outfile_ok = "Xmatched.fasta.gz"
 
 function test()
     outfile_tst = tempname()
+    outfile_harmonized = tempname()
     try
-        paralog_matching(testfile1, testfile2, outfile_tst, cutoff=4, pseudo_count=0.8, batch=5)
-
+        X12, match = paralog_matching(testfile1, testfile2, outfile_tst, cutoff=4, pseudo_count=0.8, batch=5)
+        X = ParalogMatching.Alignment(X12, match)
+        ParalogMatching.write_fasta(X, outfile_harmonized)
         M1 = readfasta(outfile_tst)
         M2 = readfasta(outfile_ok)
+        Mharmo = readfasta(outfile_harmonized)
         @test M1 == M2
+        @test M1 == Mharmo
     finally
         rm(outfile_tst)
+        rm(outfile_harmonized)
     end
 end
 
